39365a906e2c5179a181636c00a64fcc19a0f60f
DariaDusheiko <74820743+DariaDusheiko@users.noreply.github.com>
2024-09-30 00:23:18 +0300

    first class with right numbers

--- first_part.cpp
+++ first_part.cpp
@@ -13,6 +13,22 @@ class Car_line {
         Car_line(double spd, double accel, double turn, double startX, double startY, double time);
         Car_line(const Car_line& other);
 
+        void changeSpeed(double spd);
+        void changeAcceleration(double accel);
+        void changeTurnAngle(double turn);
+        void changeX(double startX);
+        void changeY(double startY);
+        void changeTime(double time);
+
+        double getPositionX() const { return positionX; }
+        double getPositionY() const { return positionY; }
+        double getSpeed() const { return speed; }
+        double getAcceleration() const { return acceleration; }
+        double getAngle() const { return turnAngle; }
+        double getTime() const { return startTime; }
+
+
+    private:
         double speed;      
         double acceleration; 
         double turnAngle;  
@@ -33,6 +49,18 @@ Car_line::Car_line()
 
 Car_line::Car_line(double spd, double accel, double turn, double startX, double startY, double time)
 {
+    if (spd < 0) {
+        spd = 0.0;
+    }
+        
+    if (turn < -360 || turn > 360) {
+        turn = 0.0;
+    }
+        
+    if (time < 0) {
+        time = 0.0;
+    }
+
     speed = spd;
     acceleration = accel;
     turnAngle = turn;
@@ -52,37 +80,77 @@ Car_line::Car_line(const Car_line& other)
 };
 
 
+void Car_line::changeSpeed(double spd)
+{
+    if (spd < 0) {
+        spd = 0.0;
+    }
+    speed = spd;
+}
+
+void Car_line::changeAcceleration(double accel)
+{
+    acceleration = accel;
+}
+
+void Car_line::changeX(double startX)
+{
+    positionX = startX;
+}
+
+void Car_line::changeY(double startY)
+{
+    positionY = startY;
+}
+
+void Car_line::changeTurnAngle(double turn)
+{
+    if (turn < -360 || turn > 360) {
+        turn = 0.0;
+    }
+    turnAngle = turn;
+}
+void Car_line::changeTime(double time)
+{
+    if (time < 0) {
+        time = 0.0;
+    }
+    startTime = time;
+}
+
+
 int main() {
     {
         Car_line car_line;
-        assert(car_line.speed == 0.0);
-        assert(car_line.acceleration == 0.0);
-        assert(car_line.turnAngle == 0.0);
-        assert(car_line.positionX == 0.0);
-        assert(car_line.positionY == 0.0);
-        assert(car_line.startTime == 0.0);
+        assert(car_line.getSpeed() == 0.0);
+        assert(car_line.getAcceleration() == 0.0);
+        assert(car_line.getAngle() == 0.0);
+        assert(car_line.getPositionX()  == 0.0);
+        assert(car_line.getPositionY() == 0.0);
+        assert(car_line.getTime() == 0.0);
     }
 
-    Car_line car_line(45.0, -4.0, 55.0, 8.8, 0.0, 78.1);
+    Car_line car_line(-45, -4.0, 55.0, 8.8, 0.0, 78.1);
 
-    assert(car_line.speed == 45.0);
-    assert(car_line.acceleration == -4.0);
-    assert(car_line.turnAngle == 55.0);
-    assert(car_line.positionX == 8.8);
-    assert(car_line.positionY == 0.0);
-    assert(car_line.startTime == 78.1);
+    assert(car_line.getSpeed() == 0.0);
+    assert(car_line.getAcceleration() == -4.0);
+    assert(car_line.getAngle() == 55.0);
+    assert(car_line.getPositionX()  == 8.8);
+    assert(car_line.getPositionY() == 0.0);
+    assert(car_line.getTime() == 78.1);
 
-    car_line.speed = 70.0;
-    car_line.acceleration = 9.0;
-    car_line.positionX = 7.6;
+    car_line.changeSpeed(70.0);
+    car_line.changeAcceleration(9.0);
+    car_line.changeX(7.6);
+    car_line.changeTime(-78.1);
     Car_line car_line_new = car_line;
 
-    assert(car_line_new.speed == 70.0);
-    assert(car_line_new.acceleration == 9.0);
-    assert(car_line_new.turnAngle == 55.0);
-    assert(car_line_new.positionX == 7.6);
-    assert(car_line_new.positionY == 0.0);
-    assert(car_line_new.startTime == 78.1);
+    assert(car_line_new.getSpeed() == 70.0);
+    assert(car_line_new.getAcceleration() == 9.0);
+    assert(car_line_new.getAngle() == 55.0);
+    assert(car_line_new.getPositionX()  == 7.6);
+    assert(car_line_new.getPositionY() == 0.0);
+    assert(car_line_new.getTime() == 0.0);
 
     cout << "All tests done!" << endl;
     return 0;
